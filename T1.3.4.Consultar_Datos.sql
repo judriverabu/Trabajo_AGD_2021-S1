0fe0221a09666851afdaac58f4c8de35c2d63890
Daniel Cardona Velasquez <dacardonave@unal.edu.co>
2021-05-21 18:38:01 -0500

    consultas-2

--- T1.3.4.Consultar_Datos.sql
+++ T1.3.4.Consultar_Datos.sql
@@ -19,3 +19,16 @@ GROUP BY 1;
 SELECT IS_PRIME, count(application_user_id) as usuarios
 FROM user_entity
 GROUP BY 1;
+
+--Conteo de ordenes por genero de los usuarios
+SELECT a.GENDER, COUNT(b.ORDER_ID) as Num_Ordenes
+From user_entity as a INNER JOIN order_entity as b
+ON  a.APPLICATION_USER_ID = b.APPLICATION_USER_ID
+GROUP BY a.GENDER;
+
+
+--Promedio del valor total de las ordenes por dispotivio de registro de usuario
+SELECT a.REGISTER_DEVICE, AVG(b.TOTAL_VALUE) as Valor_Ordenes
+From user_entity as a JOIN order_entity as b
+ON  a.APPLICATION_USER_ID = b.APPLICATION_USER_ID
+GROUP BY a.REGISTER_DEVICE;
